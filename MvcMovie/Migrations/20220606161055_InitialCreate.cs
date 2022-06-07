5bc1a1bcee34c8b71013f133e33dc7843ed6aae4
fperez <PEREZFAQ98-2020@ies21.onmicrosoft.com>
2022-06-07 11:26:18 -0300

    Agregar archivos de proyecto.

---
+++ MvcMovie/Migrations/20220606161055_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) //crea la tabla Movie y configura Id como la clave principal.
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) // revierte los cambios de esquema realizados por la migración Up.
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; } //el "?" indica que la propiedad admite un valor NULL 
+
+        [Display(Name="Release Date")]
+        [DataType(DataType.Date)] //Con el atributo date el usuario no tiene que especificar informacion horaria en el campo fecha.
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; } //Admite valor null
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
